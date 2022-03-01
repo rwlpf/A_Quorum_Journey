45a11aa82f459f1a53c16089f314a872b2aa7d66
French <rwlp.french@gmail.com>
2022-03-01 11:04:55 +0000

    Updates

---
+++ Quroum_Round_USA/Stage1/Calulate_Distance_Stage1_step_2_v2.sql
@@ -0,0 +1,66 @@
+
+IF EXISTS(SELECT *
+          FROM   [dbo].[Journey_USA_Stage1])
+  TRUNCATE TABLE [dbo].[Journey_USA_Stage1];
+
+;
+WITH Step2
+AS (SELECT [idx],
+           [lat],
+           [lon],
+           [ele]
+    FROM [dbo].[Journey_USA_Stage1_Step1]
+	ORDER BY [idx]
+   ),
+     Step3
+AS (SELECT [idx],
+           [lat],
+           [lon],
+           [ele],
+           LEAD(step2.[lat], 1) OVER (ORDER BY [idx]) AS [Lat_end],
+           LEAD(step2.[lon], 1) OVER (ORDER BY [idx]) AS [Lon_end],
+           ([lat] / 180) * PI() AS Lat_Start_Rad,
+           ([lon] / 180) * PI() AS Lon_Start_Rad,
+           (LEAD(step2.[lat], 1) OVER (ORDER BY [idx]) / 180) * PI() AS [Lat_end_Rad],
+           (LEAD(step2.[lon], 1) OVER (ORDER BY [idx]) / 180) * PI() AS [Lon_end_Rad]
+    FROM step2
+   )
+INSERT INTO [dbo].[Journey_USA_Stage1]
+(
+    [Index],
+    [Lat_Start],
+    [Lon_Start],
+    [Elevation],
+    [Lat_End],
+    [Lon_End],
+    [Lat_Start_Rad],
+    [Lon_Start_Rad],
+    [Lat_End_Rad],
+    [Lon_End_Rad],
+    [Distance_Km],
+    [RT_Km],
+    [PointSize]
+)
+SELECT [idx],
+       [lat],
+       [lon],
+       [ele],
+       [Lat_end],
+       [Lon_end],
+       Lat_Start_Rad,
+       Lon_Start_Rad,
+       [Lat_end_Rad],
+       [Lon_end_Rad],
+       ACOS(SIN(Lat_Start_Rad) * SIN([Lat_end_Rad]) + COS([Lat_Start_Rad]) * COS([Lat_end_Rad])
+            * COS([Lon_end_Rad] - [Lon_Start_Rad])
+           ) * 6371 AS Distance_Km,
+       SUM(ACOS(SIN(Lat_Start_Rad) * SIN([Lat_end_Rad]) + COS([Lat_Start_Rad]) * COS([Lat_end_Rad])
+                * COS([Lon_end_Rad] - [Lon_Start_Rad])
+               ) * 6371
+          ) OVER (ORDER BY [idx]) AS RT_Km,
+       1 as PointSize
+FROM Step3
+WHERE [Lat_end] IS NOT NULL
+
+DROP TABLE #SubSetOfRecords;
+DROP TABLE #Tracks;
